59b43f3306a1ae2dcbc61016e50fcafd192aff9e
Denis Sadokha <sadoha@goodline.info>
2020-12-14 12:27:54 +0700

    final sprint

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Nav from './src/component/Navig';
+
+
+
+
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Nav);
